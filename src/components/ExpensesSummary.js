d712367f6e6c1a3586dc095128d4ddff403428fb
Peter Madziyire <petertm@live.co.uk>
2019-11-29 07:15:12 +0000

    Add Expense Summary to Dashboard

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+const ExpensesSummary = ({expensesCount, expensesTotal})=> {
+    const formattedExpensesTotal = numeral(expensesTotal/100).format('0,0.00');
+
+    return (
+        <div>
+        <h1> Viewing {expensesCount} expenses totalling {formattedExpensesTotal}</h1>
+        </div>
+
+
+    );
+
+};
+
+const mapStateToProps =(state)=> {
+
+const visibleExpenses= selectExpenses(state.expenses, state.filters);
+
+return {
+expensesCount: visibleExpenses.length,
+expensesTotal: selectExpensesTotal(visibleExpenses)
+};
+
+
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
---
+++ src/components/locales/fr.js
@@ -0,0 +1,22 @@
+// load a locale
+numeral.register('locale', 'fr', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '€'
+    }
+});
+
+// switch between locales
+numeral.locale('fr');
